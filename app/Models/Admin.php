c3b13a7730711294530c910e9453a368459198d8
mohammed marey <mohammedmaray9674@gmail.com>
2022-12-10 15:17:45 +0200

    edit admin table

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
